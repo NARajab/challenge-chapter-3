3cc37dd807bfb9b1708531ff07a2c40e4eba5eec
NARajab <11211067@student.itk.ac.id>
2023-09-21 12:40:07 +0800

    added the morgan package and refactored the routing in the app.js file

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
 const express = require("express");
 const fs = require("fs");
-
+const morgan = require("morgan");
 const app = express();
 
+app.use(morgan("dev"));
 // Midelware
 app.use(express.json());
 
 // Port
 const PORT = process.env.PORT || 8000;
 
-const cars = JSON.parse(fs.readFileSync(`${__dirname}/data/cars.json`));
+const cars = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/cars.json`)
+);
 
-app.get("/", (req, res) => {
+const pingSuccessfully = (req, res) => {
   res.status(200).json({
     message: "ping successfully",
   });
-});
+};
 
-app.get("/api/v1/cars", (req, res) => {
+const getAllCars = (req, res) => {
   res.status(200).json({
     status: "success",
     data: {
       cars,
     },
   });
-});
+};
 
-app.get("/api/v1/cars/:id", (req, res) => {
+const getCarById = (req, res) => {
   const id = req.params.id;
   console.log(id);
   const car = cars.find((el) => el.id === id);
@@ -43,26 +46,35 @@ app.get("/api/v1/cars/:id", (req, res) => {
       car,
     },
   });
-});
+};
 
-app.post("/api/v1/cars", (req, res) => {
+const createCar = (req, res) => {
   const newId = cars[cars.length - 1].id + 1;
-  const newData = Object.assign({ id: newId }, req.body);
+  const newData = Object.assign(
+    { id: newId },
+    req.body
+  );
 
   cars.push(newData);
-  fs.writeFile(`${__dirname}/data/cars.json`, JSON.stringify(cars), (err) => {
-    res.status(201).json({
-      status: "success",
-      data: {
-        car: newData,
-      },
-    });
-  });
-});
+  fs.writeFile(
+    `${__dirname}/data/cars.json`,
+    JSON.stringify(cars),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          car: newData,
+        },
+      });
+    }
+  );
+};
 
-app.put("/api/v1/cars/:id", (req, res) => {
+const updateCar = (req, res) => {
   const id = req.params.id;
-  const carIndex = cars.findIndex((el) => el.id === id);
+  const carIndex = cars.findIndex(
+    (el) => el.id === id
+  );
 
   if (carIndex === -1) {
     return res.status(400).json({
@@ -70,19 +82,28 @@ app.put("/api/v1/cars/:id", (req, res) => {
       message: `Data with ${id} its not found`,
     });
   }
-  cars[carIndex] = { ...cars[carIndex], ...req.body };
+  cars[carIndex] = {
+    ...cars[carIndex],
+    ...req.body,
+  };
 
-  fs.writeFile(`${__dirname}/data/cars.json`, JSON.stringify(cars), (err) => {
-    res.status(200).json({
-      status: "success",
-      message: `This car data with id ${id} has been edited`,
-    });
-  });
-});
+  fs.writeFile(
+    `${__dirname}/data/cars.json`,
+    JSON.stringify(cars),
+    (err) => {
+      res.status(200).json({
+        status: "success",
+        message: `This car data with id ${id} has been edited`,
+      });
+    }
+  );
+};
 
-app.delete("/api/v1/cars/:id", (req, res) => {
+const deleteCar = (req, res) => {
   const id = req.params.id;
-  const carIndex = cars.findIndex((el) => el.id === id);
+  const carIndex = cars.findIndex(
+    (el) => el.id === id
+  );
 
   if (carIndex === -1) {
     return res.status(400).json({
@@ -92,15 +113,40 @@ app.delete("/api/v1/cars/:id", (req, res) => {
   }
   cars.splice(carIndex, 1);
 
-  fs.writeFile(`${__dirname}/data/cars.json`, JSON.stringify(cars), (err) => {
-    res.status(200).json({
-      status: "success",
-      message: "Data has been deleted",
-      data: null,
-    });
-  });
-});
+  fs.writeFile(
+    `${__dirname}/data/cars.json`,
+    JSON.stringify(cars),
+    (err) => {
+      res.status(200).json({
+        status: "success",
+        message: "Data has been deleted",
+        data: null,
+      });
+    }
+  );
+};
+
+const carPingRouter = express.Router();
+const carRouter = express.Router();
+
+carPingRouter.route("/").get(pingSuccessfully);
+
+carRouter
+  .route("/")
+  .get(getAllCars)
+  .post(createCar);
+
+carRouter
+  .route("/:id")
+  .get(getCarById)
+  .put(updateCar)
+  .delete(deleteCar);
+
+app.use("/", carPingRouter);
+app.use("/api/v1/cars", carRouter);
 
 app.listen(PORT, () => {
-  console.log(`App running into http://localhost:${PORT}...`);
+  console.log(
+    `App running into http://localhost:${PORT}...`
+  );
 });
